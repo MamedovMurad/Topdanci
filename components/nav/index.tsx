dc6b6d9253f3bebab015f0e0803ee3eae494833d
MamedovMurad <56360710+MamedovMurad@users.noreply.github.com>
2023-03-30 18:30:42 +0400

    added butun elanlar

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -7,35 +7,35 @@ type NavProps = {};
 const Nav: React.FC<NavProps> = () => {
   return (
     <nav className={styles.nav}>
-<div className="wrapper">
-<ul>
-        <li className={styles.logo} onClick={()=>Router.push('/')}>
-          <a>topdancı.az</a> <p>Topdan Al/Topdan Sat</p>
-        </li>
-        <li className={styles.navbar}>
-          <ul>
-            <li>
-              <a href="">Bütün elanlar</a>
-            </li>
-            <li>
-              <Link href={"/topdancilar/"}>Topdançılar</Link>
-              
-            </li>
-            <li>
-            <Link href={"/alicilar"}>Alıcılar</Link>
-           
-            </li>
-            <li>
-            <Link href={"/saticilar/"}>Satıcılar</Link>
-          
-            </li>
-          </ul>
-        </li>
-        <li className={styles.rightButton}>
-          <PrimaryButton text="YENİ ELAN" onClick={()=>Router.push('yeni-elan')} />
-        </li>
-      </ul>
-</div>
+      <div className="wrapper">
+        <ul>
+          <li className={styles.logo} onClick={() => Router.push('/')}>
+            <a>topdancı.az</a> <p>Topdan Al/Topdan Sat</p>
+          </li>
+          <li className={styles.navbar}>
+            <ul>
+              <li>
+                <Link href={"/"}>Bütün elanlar</Link>
+              </li>
+              <li>
+                <Link href={"/topdancilar/"}>Topdançılar</Link>
+
+              </li>
+              <li>
+                <Link href={"/alicilar"}>Alıcılar</Link>
+
+              </li>
+              <li>
+                <Link href={"/saticilar/"}>Satıcılar</Link>
+
+              </li>
+            </ul>
+          </li>
+          <li className={styles.rightButton}>
+            <PrimaryButton text="YENİ ELAN" onClick={() => Router.push('yeni-elan')} />
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
