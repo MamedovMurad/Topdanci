866de53b9aaee18f33a8115e1b028beaad4689fe
MamedovMurad <56360710+MamedovMurad@users.noreply.github.com>
2023-04-01 02:05:39 +0400

    add items with animations for dashboard

--- container/profile/adverts/index.tsx
+++ container/profile/adverts/index.tsx
@@ -10,7 +10,7 @@ interface AdvertsProps {
 
 const Adverts: React.FC<AdvertsProps> = ({ }) => {
     const [link, setlink] = useState('active')
-    const [data, setdata] = useState<any>({ active: [], pending: [], rejected: [], expired: [] })
+    const [data, setdata] = useState<any>({ active: null, pending: null, rejected: null, expired: null })
     const links = [
         { title: 'Aktiv', count: 0, id: 'active' },
         { title: 'Gözləmədə olan', count: 0, id: 'pending' },
@@ -19,18 +19,20 @@ const Adverts: React.FC<AdvertsProps> = ({ }) => {
 
     async function fetchData(params: string, object: string) {
         const response = await api.get(params)
-        setdata({
-            ...data, [object]: response.data.adverts.map((item: any) => <ProductCard key={item.id} data={{
-                id: item.id,
-                title: item.title,
-                address: "Bakı",
-                minNumber: item.min_order,
-                price: item.wholesale_price,
-                date: item.date,
-                photo: item.image?.src,
-                label: item.advert_type ? "buyer" : 'seller',
-                type: "premium"
-            }} />)
+        setdata((prev: any) => {
+            return {
+                ...prev, [object]: response.data.adverts.map((item: any) => <ProductCard key={item.id} data={{
+                    id: item.id,
+                    title: item.title,
+                    address: "Bakı",
+                    minNumber: item.min_order,
+                    price: item.wholesale_price,
+                    date: item.date,
+                    photo: item.image?.src,
+                    label: item.advert_type ? "buyer" : 'seller',
+                    type: "premium"
+                }} />)
+            }
         })
     }
     useEffect(() => {
@@ -39,20 +41,22 @@ const Adverts: React.FC<AdvertsProps> = ({ }) => {
         fetchData('user/rejected-adverts', 'rejected')
         fetchData('user/active-adverts', 'active')
     }, [])
+    console.log(data);
 
     return (
         <div className={styles.adverts}>
+
             <ul className={styles.links}>
                 {links.map(item => (
                     <li key={item.title} onClick={() => setlink(item.id)}
-                        style={item.id === link ? { color: '#E61C23' } : {}}>{item.title + ' (' + data[item.id].length + ')'}</li>
+                        style={item.id === link ? { color: '#E61C23' } : {}}>{item.title + ' (' + (data[item.id]?.length || 0) + ')'}</li>
                 ))}
             </ul>
             <main className={styles.advertsMain}>
-                {!data[link].length && <EmptyAdverts title={links.find((item) => item.id === link)?.title.toLocaleLowerCase() || 'aktiv'} />}
+                {data[link]?.length < 1 && <EmptyAdverts title={links.find((item) => item.id === link)?.title.toLocaleLowerCase() || 'aktiv'} />}
                 {
-                    <ProductsContainer list={data[link]}/>
-                   
+                    <ProductsContainer list={data[link]} />
+
                 }
             </main>
 
