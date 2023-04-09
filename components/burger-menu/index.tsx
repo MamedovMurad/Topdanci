13a2067128b0617931cb36657c9c464cb5ff4cd6
MamedovMurad <56360710+MamedovMurad@users.noreply.github.com>
2023-04-10 02:18:42 +0400

    add burger menu active page

--- components/burger-menu/index.tsx
+++ components/burger-menu/index.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import styles from './index.module.css';
+import Link from 'next/link';
+import { UserLoginSVG } from '../../assets/svg/userloginsvg';
+import useModal from '../../hooks/useModal';
 interface BurgerMenuProps {
-  
+  closeModal:()=>void
 }
 
-const BurgerMenu: React.FC<BurgerMenuProps> = ({ }) => {
+const BurgerMenu: React.FC<BurgerMenuProps> = ({closeModal }) => {
+ 
   return (
-    <div className={styles.burgerMenuParent}> 
-        <div className={styles.burgerMenu}></div>
+    <div className={styles.burgerMenuParent} onClick={closeModal}> 
+        <div className={styles.burgerMenu}>
+          <header></header>
+          <main>
+            <ul>
+              <li><span><UserLoginSVG/></span> <Link href={''}>Giriş</Link></li>
+              <li><span><UserLoginSVG/></span> <Link href={''}>Balans artımı</Link></li>
+              <li><span><UserLoginSVG/></span> <Link href={''}>Dəstək</Link></li>
+            </ul>
+            <ul>
+              <li> <Link href={''}>Layihə haqqında</Link></li>
+              <li> <Link href={''}>Ödənişli xidmətlər</Link></li>
+              <li> <Link href={''}>Paketlər</Link></li>
+              <li> <Link href={''}>Saytda reklam</Link></li>
+            </ul>
+            <ul className={styles.links}>
+              <li>Əlaqə</li>
+              <li><span><UserLoginSVG/></span> <Link href={''}>Facebook</Link></li>
+              <li><span><UserLoginSVG/></span> <Link href={''}>Instagram</Link></li>
+              <li><span><UserLoginSVG/></span> <Link href={''}>Linkedin</Link></li>
+              <li><span><UserLoginSVG/></span> <Link href={''}>Youtube</Link></li>
+            </ul>
+          </main>
+        </div>
     </div>
   );
 };
--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -5,50 +5,56 @@ import styles from "./index.module.css";
 import Link from "next/link";
 import { BurgerSVG } from "../../assets/svg/burger";
 import BurgerMenu from "../burger-menu";
+import useModal from "../../hooks/useModal";
 type NavProps = {};
 
 const Nav: React.FC<NavProps> = () => {
   const router = useRouter()
-
+  const { isOpen,
+    openModal,
+    closeModal } = useModal()
 
   return (
-    <nav className={styles.nav}>
-     {/*  <BurgerMenu/> */}
-      <div className="wrapper">
-        <ul>
-          <li className={styles.burgerMenu}>
-            <div>
-              <BurgerSVG/>
-            </div>
-          </li>
-          <li className={styles.logo} onClick={() => Router.push('/')}>
-            <a>topdancı.az</a> <p>Topdan Al/Topdan Sat</p>
-          </li>
-          <li className={styles.navbar}>
-            <ul>
-              <li>
-                <Link href={"/"}>Bütün elanlar</Link>
-              </li>
-              <li>
-                <Link href={"/topdancilar/"}>Topdançılar</Link>
+    <>
+    {isOpen&&  <BurgerMenu  closeModal={closeModal}/>}
+      <nav className={styles.nav}>
 
-              </li>
-              <li>
-                <Link href={{ pathname: router.pathname, search: `?${stringify({ ...router.query, advert_type: 1 })}` }}>Alıcılar</Link>
+        <div className="wrapper">
+          <ul>
+            <li className={styles.burgerMenu} onClick={openModal}>
+              <div>
+                <BurgerSVG />
+              </div>
+            </li>
+            <li className={styles.logo} onClick={() => Router.push('/')}>
+              <a>topdancı.az</a> <p>Topdan Al/Topdan Sat</p>
+            </li>
+            <li className={styles.navbar}>
+              <ul>
+                <li>
+                  <Link href={"/"}>Bütün elanlar</Link>
+                </li>
+                <li>
+                  <Link href={"/topdancilar/"}>Topdançılar</Link>
 
-              </li>
-              <li>
-                <Link href={{ pathname: router.pathname, search: `?${stringify({ ...router.query, advert_type: 0 })}` }} >Satıcılar</Link>
+                </li>
+                <li>
+                  <Link href={{ pathname: router.pathname, search: `?${stringify({ ...router.query, advert_type: 1 })}` }}>Alıcılar</Link>
 
-              </li>
-            </ul>
-          </li>
-          <li className={styles.rightButton}>
-            <PrimaryButton text="YENİ ELAN" onClick={() => Router.push('yeni-elan')} />
-          </li>
-        </ul>
-      </div>
-    </nav>
+                </li>
+                <li>
+                  <Link href={{ pathname: router.pathname, search: `?${stringify({ ...router.query, advert_type: 0 })}` }} >Satıcılar</Link>
+
+                </li>
+              </ul>
+            </li>
+            <li className={styles.rightButton}>
+              <PrimaryButton text="YENİ ELAN" onClick={() => Router.push('yeni-elan')} />
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 };
 
