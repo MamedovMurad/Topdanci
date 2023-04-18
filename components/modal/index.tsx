4bfcc709cf5a719e5d7b12535693908c637f6dc3
MamedovMurad <56360710+MamedovMurad@users.noreply.github.com>
2023-04-19 00:32:57 +0400

    add swipe method for mobile detail page

--- components/modal/index.tsx
+++ components/modal/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import styles from './index.module.css';
+import { CancelSVG } from '../../assets/svg/cancel';
+import { useSwipeable } from 'react-swipeable';
 interface ModalProps {
   closeModal?: () => void,
   pathes: { src: string, alt: string }[],
@@ -9,29 +11,39 @@ interface ModalProps {
 
 const Modal: React.FC<ModalProps> = ({ closeModal, pathes, index }) => {
   const [selected, setselected] = useState(index)
-  function handleClick(event: any) {
-    event.stopPropagation();
+  
+  function handleClick(event?: any) {
+    event&& event.stopPropagation();
     if (selected > 0) {
       setselected(selected - 1)
     } else {
       setselected(pathes.length - 1)
     }
   }
-  function handleClickRight(event: any) {
-    event.stopPropagation();
+  function handleClickRight(event?: any) {
+    event&& event.stopPropagation();
     if (pathes.length - 1 > selected) {
       setselected(selected + 1)
     } else {
       setselected(index)
     }
   }
-  console.log(selected, 'index');
+  const handlers = useSwipeable({
+    onSwiped: (eventData) =>{
+     if (eventData.dir==='Right') {
+      return handleClickRight()
+     }
+     return handleClick()
+    },
+
+  });
 
   return (
     <section className={styles.modalParent} onClick={closeModal}>
       <div className={styles.modal} onClick={(event: any) => event.stopPropagation()}>
+        <span className={styles.modalCancel} onClick={closeModal}><CancelSVG/></span>
         <div className={styles.leftArrow} onClick={handleClick}></div>
-        <img src={pathes[selected].src} alt="" />
+        <img src={pathes[selected].src} alt="" {...handlers}/>
         <div className={styles.rightArrow} onClick={handleClickRight}></div>
       </div>
     </section>
