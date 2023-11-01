acb3a483cb404cfbd94a3dbaeee954e7a851b90e
MamedovMurad <56360710+MamedovMurad@users.noreply.github.com>
2023-11-01 22:46:21 +0400

    fixed cards of product container

--- components/companyItem/index.tsx
+++ components/companyItem/index.tsx
@@ -1,40 +1,45 @@
-import React from 'react';
-import styles from './index.module.css';
-import Image from 'next/image';
-import Router from 'next/router';
-import { MyComponent } from '../../hooks/useResponsivenenessAdjuster';
+import React from "react";
+import styles from "./index.module.css";
+import Image from "next/image";
+import Router from "next/router";
+import { MyComponent } from "../../hooks/useResponsivenenessAdjuster";
 interface CompanyItemProps {
-    title: string,
-    src: string,
-    desc: string,
-    tel:string,
-    count:number,
-    id:number
+  title: string;
+  src: string;
+  desc: string;
+  tel: string;
+  count: number;
+  id: number;
 }
 
-const CompanyItem: React.FC<CompanyItemProps> = ({ title, src, desc,tel,count,id }) => {
-    const responsive  = MyComponent()
-    return (
-        <div className={styles.companyitem} onClick={()=>Router.push('/topdanci/'+id)}>
-            <div className={styles.logo}>
-                <Image
-                    src={src}
-                    alt={title}
-                   fill
-                />
-            </div>
-            <div className={styles.content  }>
-                <h5>{title}</h5>
-                <p>
-                    {responsive<900?desc.slice(0,60)   :desc} </p>
-                <a href={"tel:"+tel}>{tel}</a>
+const CompanyItem: React.FC<CompanyItemProps> = ({
+  title,
+  src,
+  desc,
+  tel,
+  count,
+  id,
+}) => {
+  const responsive = MyComponent();
+  return (
+    <div
+      className={styles.companyitem}
+      onClick={() => Router.push("/topdanci/" + id)}
+    >
+      <div className={styles.logo}>
+        <Image src={src} alt={title} fill />
+      </div>
+      <div className={styles.content}>
+        <h5>{title}</h5>
+        <p>{responsive < 900 ? desc.slice(0, 60) : desc} </p>
+        <a href={"tel:" + tel}>{tel}</a>
 
-                <div>
-                    <span>{count} Elan</span>
-                </div>
-            </div>
+        <div>
+          <span>{count} Elan</span>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CompanyItem;
