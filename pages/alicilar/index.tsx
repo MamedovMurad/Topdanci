36415f73a99033e9f4ad996b5af0e7f41e136654
MamedovMurad <56360710+MamedovMurad@users.noreply.github.com>
2023-11-04 12:43:32 +0400

    fixed profile page

--- pages/alicilar/index.tsx
+++ pages/alicilar/index.tsx
@@ -4,42 +4,51 @@ import ProductsContainer from "../../container/products";
 import { api } from "../../common/api";
 import ProductCard from "../../components/card/product";
 
-type AliciPageProps = {}
+type AliciPageProps = {};
 
 const Alici: React.FC<AliciPageProps> = () => {
-    const [products, setproducts] = useState([])
-    async function fetchProducts() {
-        const response = await api.get('adverts?advert_type=1')
-        setproducts(response.data.adverts.map((item: any, index: number) => (
-            <ProductCard
-                key={index}
-                data={{
-                    id: item.id,
-                    title: item.title,
-                    address: "Bakı",
-                    minNumber: item.min_order,
-                    price: item.wholesale_price,
-                    date: item.date,
-                    photo: item.image?.src,
-                    label: item.advert_type ? "buyer" : 'seller',
-                    type: "premium"
-                }}
-            />
-        )))
-        console.log(response);
-
-
-    }
-    useEffect(() => {
-        fetchProducts()
-    }, [])
-    return (
-        <div>
-            <Menu />
-            <div className="wrapper"> <ProductsContainer seletedUrl="/alicilar" title="Alıcılar" isproduct style={{ transform: 'translateY(-100px)' }} list={products} /></div>
-        </div>
+  const [products, setproducts] = useState([]);
+  async function fetchProducts() {
+    const response = await api.get("adverts?advert_type=1");
+    setproducts(
+      response.data.adverts.map((item: any, index: number) => (
+        <ProductCard
+          key={index}
+          data={{
+            id: item.id,
+            title: item.title,
+            address: "Bakı",
+            minNumber: item.min_order,
+            price: item.wholesale_price,
+            date: item.date,
+            photo: item.image?.src,
+            label: item.advert_type ? "buyer" : "seller",
+            type: item.premium == 1 ? "premium" : "ordinary",
+            wholersalers: item.official_wholesaler,
+          }}
+        />
+      ))
     );
-}
-
+    console.log(response);
+  }
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  return (
+    <div>
+      <Menu />
+      <div className="wrapper">
+        {" "}
+        <ProductsContainer
+          seletedUrl="/alicilar"
+          title="Alıcılar"
+          isproduct
+          style={{ transform: "translateY(-100px)" }}
+          list={products}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Alici;
--- pages/saticilar/index.tsx
+++ pages/saticilar/index.tsx
@@ -5,48 +5,61 @@ import { api } from "../../common/api";
 import ProductCard from "../../components/card/product";
 import { useRouter } from "next/router";
 
-type SaticiPageProps = {}
+type SaticiPageProps = {};
 
 const Satici: React.FC<SaticiPageProps> = () => {
-    const router = useRouter()
-    const search_text = router.query.search_text ? router?.query?.search_text + '' : ''
-    const city = router.query.city ? router?.query?.city + '' : ''
-    const category = router.query.category ? router?.query?.category + '' : ''
+  const router = useRouter();
+  const search_text = router.query.search_text
+    ? router?.query?.search_text + ""
+    : "";
+  const city = router.query.city ? router?.query?.city + "" : "";
+  const category = router.query.category ? router?.query?.category + "" : "";
 
-    const [products, setproducts] = useState([])
-    async function fetchProducts() {
-
-        const response = await api.get('adverts?' + new URLSearchParams({ advert_type: '0', search_text, city, category }))
-        setproducts(response.data.adverts.map((item: any, index: number) => (
-            <ProductCard
-                key={index}
-                data={{
-                    id: item.id,
-                    title: item.title,
-                    address: "Bakı",
-                    minNumber: item.min_order,
-                    price: item.wholesale_price,
-                    date: item.date,
-                    photo: item.image?.src,
-                    label: item.advert_type ? "buyer" : 'seller',
-                    type: "premium"
-                }}
-            />
-        )))
-        console.log(response);
-
-
-    }
-    useEffect(() => {
-        fetchProducts()
-    }, [router.query])
-    return (
-        <div>
-            <Menu />
-            <div className="wrapper"> <ProductsContainer seletedUrl="/saticilar" title="Satıcılar" isproduct style={{ transform: 'translateY(-100px)' }} list={products} /></div>
-        </div>
+  const [products, setproducts] = useState([]);
+  async function fetchProducts() {
+    const response = await api.get(
+      "adverts?" +
+        new URLSearchParams({ advert_type: "0", search_text, city, category })
     );
-}
-
+    setproducts(
+      response.data.adverts.map((item: any, index: number) => (
+        <ProductCard
+          key={index}
+          data={{
+            id: item.id,
+            title: item.title,
+            address: "Bakı",
+            minNumber: item.min_order,
+            price: item.wholesale_price,
+            date: item.date,
+            photo: item.image?.src,
+            label: item.advert_type ? "buyer" : "seller",
+            type: item.premium == 1 ? "premium" : "ordinary",
+            wholersalers: item.official_wholesaler,
+          }}
+        />
+      ))
+    );
+    console.log(response);
+  }
+  useEffect(() => {
+    fetchProducts();
+  }, [router.query]);
+  return (
+    <div>
+      <Menu />
+      <div className="wrapper">
+        {" "}
+        <ProductsContainer
+          seletedUrl="/saticilar"
+          title="Satıcılar"
+          isproduct
+          style={{ transform: "translateY(-100px)" }}
+          list={products}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Satici;
