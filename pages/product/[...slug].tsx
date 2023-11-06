28b42d45ae10867c4d3ac6afff0aaac604900908
MamedovMurad <56360710+MamedovMurad@users.noreply.github.com>
2023-11-06 23:34:42 +0400

    added similar adverts

--- pages/product/[...slug].tsx
+++ pages/product/[...slug].tsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import { TelIconSVG } from "../../assets/svg/tel";
 import { UserLoginSVG } from "../../assets/svg/userloginsvg";
@@ -13,32 +12,73 @@ import Router from "next/router";
 import { ShareButton } from "../../hooks/shareButton";
 import { MyComponent } from "../../hooks/useResponsivenenessAdjuster";
 import MultiSlider from "../../components/slider/multiSlider";
+import ProductCard from "../../components/card/product";
 type ProductDetailProps = {
-  data: any
+  res: any;
 };
 
-const ProductDetail: React.FC<ProductDetailProps> = ({ data }) => {
-  const { handleClick, isSupported } = ShareButton({ title: 'test', text: 'test', url: 'test' })
-const responsive  =  MyComponent()
+const ProductDetail: React.FC<ProductDetailProps> = ({ res }) => {
+  const { data, similar_adverts } = res;
+  const similar_data = similar_adverts.map((similar: any) => (
+    <ProductCard
+      key={similar.id}
+      data={{
+        id: similar.id,
+        title: similar.title,
+        address: similar.city,
+        minNumber: similar.min_order,
+        price: similar.wholesale_price,
+        date: similar.date,
+        photo: similar.image?.src,
+        label: similar.advert_type ? "buyer" : "seller",
+        type: similar.premium == 1 ? "premium" : "ordinary",
+        wholersalers: similar.official_wholesaler,
+      }}
+    />
+  ));
+  const { handleClick, isSupported } = ShareButton({
+    title: "test",
+    text: "test",
+    url: "test",
+  });
+  const responsive = MyComponent();
   return (
     <section className={styles.productPage}>
-
-      <div className={styles.alert} style={data.status.color_code ? { backgroundColor: data.status.color_code } : { padding: '0.1px' }}>
+      <div
+        className={styles.alert}
+        style={
+          data.status.color_code
+            ? { backgroundColor: data.status.color_code }
+            : { padding: "0.1px" }
+        }
+      >
         <h4>{data.status.title}</h4>
         {/*      <p></p> */}
       </div>
       <Overlay />
       <div className="wrapper full">
         <div className={styles.productPagearea}>
-            <ul className={styles.links}>
-              <li onClick={() => Router.push('/')}>Bütün elanlar</li>
-              <li onClick={() => Router.push('/?category_id=' + data.parent_category.id)}>{data?.parent_category?.name}</li>
-              <li onClick={() => Router.push('/?category_id=' + data.category.id)}>{data?.category?.name}</li>
-            </ul>
-          
-          {responsive>900?
-              <SliderUI photos={data.images} />
-          : <MultiSlider photos={data.images}/>}
+          <ul className={styles.links}>
+            <li onClick={() => Router.push("/")}>Bütün elanlar</li>
+            <li
+              onClick={() =>
+                Router.push("/?category_id=" + data.parent_category.id)
+              }
+            >
+              {data?.parent_category?.name}
+            </li>
+            <li
+              onClick={() => Router.push("/?category_id=" + data.category.id)}
+            >
+              {data?.category?.name}
+            </li>
+          </ul>
+
+          {responsive > 900 ? (
+            <SliderUI photos={data.images} />
+          ) : (
+            <MultiSlider photos={data.images} />
+          )}
 
           <div className={styles.contentTop}>
             <>
@@ -49,7 +89,7 @@ const responsive  =  MyComponent()
                   font="14px"
                   color="white"
                   bg="#E61C23"
-                  boxShadow={'none'}
+                  boxShadow={"none"}
                 />
                 <h5>{data.title}</h5>
                 <div className={styles.infocontent}>
@@ -58,39 +98,44 @@ const responsive  =  MyComponent()
                       <p>Minimum sifariş</p> <p>Topdan qiymət</p>
                     </div>
                     <div>
-                      <p className={styles.productPrice}>5 Ədəd</p> <p>100 AZN</p>
+                      <p className={styles.productPrice}>5 Ədəd</p>{" "}
+                      <p>100 AZN</p>
                     </div>
                   </div>
-                  {responsive < 900 &&<div className={styles.rightSide}>
-                    <article>
-                      Məhsul kateqoriyası:
-                    </article>
-                    <p>{data.category?.name}</p>
-                  </div>}
+                  {responsive < 900 && (
+                    <div className={styles.rightSide}>
+                      <article>Məhsul kateqoriyası:</article>
+                      <p>{data.category?.name}</p>
+                    </div>
+                  )}
                 </div>
               </div>
             </>
-            {responsive > 900 && <div className={styles.content}>
-              <article>Şəhər: Bakı</article>
-              <article>
-                Məhsul kateqoriyası: <span>{data.category?.name}</span>
-              </article>
-            </div>}
+            {responsive > 900 && (
+              <div className={styles.content}>
+                <article>Şəhər: Bakı</article>
+                <article>
+                  Məhsul kateqoriyası: <span>{data.category?.name}</span>
+                </article>
+              </div>
+            )}
             <div className={styles.buttons}>
-              {responsive > 900 && <>
-                <PrimaryButton
+              {responsive > 900 && (
+                <>
+                  <PrimaryButton
                     text="Elanı irəli çək.  1 AZN"
                     size="3px 10px"
                     font="14px"
-                />
-                <PrimaryButton
+                  />
+                  <PrimaryButton
                     text="Premium et.  5 AZN"
                     size="3px 10px"
                     color="white"
                     bg="#E61C23"
                     font="14px"
-                />
-              </>}
+                  />
+                </>
+              )}
               <div>
                 <h5>Pərakəndə satış mümkündür</h5>
                 <div>
@@ -108,8 +153,14 @@ const responsive  =  MyComponent()
             <div className={styles.personalInfo}>
               <div>
                 <div>
-                  <label ><UserLoginSVG />{data.name}</label>
-                  <p><TelIconSVG /><a href={"tel:" + data.tel}>{data.tel}</a></p>
+                  <label>
+                    <UserLoginSVG />
+                    {data.name}
+                  </label>
+                  <p>
+                    <TelIconSVG />
+                    <a href={"tel:" + data.tel}>{data.tel}</a>
+                  </p>
                 </div>
                 <ul>
                   <li> Elanın nömrəsi:35537250</li>
@@ -118,37 +169,51 @@ const responsive  =  MyComponent()
                 </ul>
               </div>
               <div className={styles.buttons}>
-                {responsive < 900 && <div className={styles.responsiveButton}>
-                  <PrimaryButton
-                      width='100%'
+                {responsive < 900 && (
+                  <div className={styles.responsiveButton}>
+                    <PrimaryButton
+                      width="100%"
                       text="Elanı irəli çək.  1 AZN"
                       size="3px 10px"
                       font="14px"
-                  />
-                  <PrimaryButton
+                    />
+                    <PrimaryButton
                       text="Premium et.  5 AZN"
-                      width='100%'
+                      width="100%"
                       size="3px 10px"
                       color="white"
                       bg="#E61C23"
                       font="14px"
+                    />
+                  </div>
+                )}
+                {responsive < 900 && (
+                  <PrimaryButton
+                    text="Paylaş"
+                    size="7px"
+                    boxShadow={"none"}
+                    onClick={isSupported && handleClick}
                   />
-                </div>}
-                {responsive < 900 &&
-                    <PrimaryButton text="Paylaş" size="7px" boxShadow={"none"} onClick={isSupported && handleClick}/>}
+                )}
               </div>
             </div>
 
             <div className={styles.moreDetail}>
               <h6>Ətraflı</h6>
-              <p>
-                {data.detail}
-              </p>
-              {responsive > 900 && <PrimaryButton text="Paylaş" size="7px" onClick={isSupported && handleClick}/>}
+              <p>{data.detail}</p>
+              {responsive > 900 && (
+                <PrimaryButton
+                  text="Paylaş"
+                  size="7px"
+                  onClick={isSupported && handleClick}
+                />
+              )}
             </div>
-
           </div>
 
+          <div>
+            <ProductsContainer title="Bənzər elanlar" list={similar_data} />
+          </div>
           {/*  <ProductsContainer  /> */}
         </div>
       </div>
@@ -158,15 +223,11 @@ const responsive  =  MyComponent()
 
 export default ProductDetail;
 
-
 export async function getServerSideProps({ params: { slug } }: any) {
   // Fetch data from external API
 
-
-  const res = await api.get('advert/' + slug[slug.length - 1])
+  const res = await api.get("advert/" + slug[slug.length - 1]);
   console.log(res);
-
-
   // Pass data to the page via props
-  return { props: { data: res.data } }
+  return { props: { res } };
 }
