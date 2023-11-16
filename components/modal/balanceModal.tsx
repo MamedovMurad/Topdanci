c5fbe984714316f90e8acfa0b365d4359fa922d0
MamedovMurad <56360710+MamedovMurad@users.noreply.github.com>
2023-11-17 00:52:46 +0400

    added modal new design

--- components/modal/balanceModal.tsx
+++ components/modal/balanceModal.tsx
@@ -7,12 +7,14 @@ import TopdanchiLogo from "../../assets/svg/Logo.svg";
 import SuccessIcon from "../../assets/svg/success";
 import { api } from "../../common/api";
 import { useRouter } from "next/router";
+import Circle from "../../assets/svg/circle.svg";
 interface ModalProps {
   closeModal?: () => void;
 }
 
 const BalanceModal: React.FC<ModalProps> = ({ closeModal }) => {
   const router = useRouter();
+  const [currentAmount, setcurrentAmount] = useState(0);
   async function handleSubmit(event: any) {
     event.preventDefault();
     api.setHeader("content-type", "application/json");
@@ -24,7 +26,7 @@ const BalanceModal: React.FC<ModalProps> = ({ closeModal }) => {
 
     const res = await api.post("user/add-balance", {
       ...result,
-      amount: result.amount,
+      amount: currentAmount,
       success_redirect_url: router.asPath + "#application-success",
       error_redirect_url: router.asPath + "#application-rejected",
     });
@@ -32,30 +34,70 @@ const BalanceModal: React.FC<ModalProps> = ({ closeModal }) => {
     return window.location.assign(res?.payment_url);
   }
 
+  const amounts = [5, 10, 20];
   return (
     <section className={styles.modalParent} onClick={closeModal}>
       <div
-        className={styles.successModal}
+        className={styles.balanceModal}
         onClick={(event: any) => event.stopPropagation()}
       >
+        <h4>Şəxsi hesabı artır</h4>
+
         <form onSubmit={handleSubmit} className={styles.balanceModalForm}>
           <div>
-            <label htmlFor="balance">Artırılacaq məbləği daxil edin.</label>
+            <label htmlFor="balance" className={styles.labelBalance}>
+              Artırılacaq məbləği daxil edin.
+            </label>
 
-            <input
-              type="number"
-              id="balance"
-              name="amount"
-              placeholder="Məbləğ. AZN"
-            />
+            <div>
+              <div className={styles.amountSpan}>
+                {amounts.map((item) => (
+                  <span
+                    key={item}
+                    className={currentAmount == item ? styles.active : ""}
+                    onClick={() => setcurrentAmount(item)}
+                  >
+                    {item} AZN
+                  </span>
+                ))}
+
+                <input
+                  type="number"
+                  id="balance"
+                  name="amount"
+                  placeholder="Məbləğ. AZN"
+                  onChange={(e: any) => setcurrentAmount(e.target.value)}
+                />
+              </div>
+              <div className={styles.paymentTYpe}>
+                <h5>Ödəniş üsulu</h5>
+                <ul>
+                  <li>
+                    {" "}
+                    <Image
+                      src={Circle}
+                      alt="bank karti "
+                      width={16}
+                      height={16}
+                    />{" "}
+                    Bank kartı
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
 
           <button> Ödə</button>
+
+          <p className={styles.bottomP}>
+            “Ödə” düyməsini sıxdıqdan sonra siz Topdanchi.az-ın <br />{" "}
+            istifadəçi razılaşmasını və ödəniş şərtlərini qəbul etmiş olursunuz
+          </p>
         </form>
 
-        <span className={styles.successModalCancel} onClick={closeModal}>
+        {/*      <span className={styles.successModalCancel} onClick={closeModal}>
           <CancelSVG color="black" />
-        </span>
+        </span> */}
       </div>
     </section>
   );
